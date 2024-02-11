084d69c613122a703ded456cffa8869fb76053ae
Tara <hymerstara@gmail.com>
2024-02-10 20:43:41 -0700

    add GET route to show the form

--- express_server.js
+++ express_server.js
@@ -13,7 +13,11 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+// GET route to show form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+//New route to render urls_show.ejs
 app.get("/urls/:id", (req, res) => {
   const shortID = req.params.id;
   const longURL = urlDatabase[shortID]; // Accessing the long URL using the short ID from urlDatabase
@@ -21,17 +25,17 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+// //additional endpoints
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+// //send HTML code
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
