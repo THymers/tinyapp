ba7c36bfa9b977c3cf2f11f8f6b9986bdb98382c
Tara <hymerstara@gmail.com>
2024-02-14 21:50:22 -0700

    add logout and remove cookies

--- express_server.js
+++ express_server.js
@@ -160,6 +160,12 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//post to logout and clear cookies
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
